65c6af06c4949cc5e6fca5be8c7865f8d2f085ba
Shreya Tripathi <tripathishreya1108@gmail.com>
2024-12-11 18:19:07 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://event-weddings-1.onrender.com/api/v1/message/send",
         {
           name,
           email,
